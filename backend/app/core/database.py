8eef31f9f6c415f7417f7c3048b4e7105fd21c12
Kien <ki3n98@gmail.com>
2025-12-09 17:10:03 -0800

    swap to supabase

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,10 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+from sqlalchemy.pool import NullPool
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@db:5432/veriface_db'
+import os
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
 
 Sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
